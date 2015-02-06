a8224a2a09d58e5d714c8ec28eeadc059795dca3
Janimatti Ellonen <janimatti.ellonen@soprano.fi>
2015-02-06 23:33:47 +0200

    Added missing argument to mapBooks

--- app/controllers/AuthorController.scala
+++ app/controllers/AuthorController.scala
@@ -92,7 +92,7 @@ class AuthorController @Inject() (implicit val env: Environment[User, CachedCook
 		Ok(Json.toJson(data))
 	}
 
-  def mapBooks(books:List[(Int, String)]) = {
+  def mapBooks(books:List[(Int, String, Boolean)]) = {
       for (book <- books) yield Map("id" -> Json.toJson(book._1), "title" -> Json.toJson(book._2), "isRead" -> Json.toJson(book._3))
   }
 	

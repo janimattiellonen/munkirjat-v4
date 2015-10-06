a0fb2218ad48683f7aeb8c5eeefec6202cf5d561
Janimatti Ellonen <janimatti.ellonen@fraktio.fi>
2015-10-06 14:38:41 +0300

    - authors once again in two columns

--- src/components/dumb/AuthorsView.js
+++ src/components/dumb/AuthorsView.js
@@ -6,23 +6,27 @@ export default React.createClass({
 
 	render() {
 
-		//let authors = _.chunk(this.props.authors.toArray(), Math.ceil(this.props.authors.count() / 2));
-
-		const {authors} = this.props;
+		let authors = _.chunk(this.props.authors.toArray(), Math.ceil(this.props.authors.count() / 2));
 
 		return (
 			<div className="component">
 				<h1>Authors</h1>
 				<div className="data-list">
-					<ul>
-					{authors.map(author => {
-						return(<li key={author.id}><a href={"/#/author/" + author.id}>{author.name} ({author.amount})</a></li>)	
+					{authors.map(set => {
+						return (
+							<ul>
+								{set.map(author => {
+									return(<li key={author.id}><a href="#">{author.name}</a></li>)
+								})}
+							</ul>
+						)
+
 					})}
-					</ul>
 				</div>
 			</div>
 		);
 	},
+
 
 	componentDidMount() {
 		//this.props.fetchAuthors();
